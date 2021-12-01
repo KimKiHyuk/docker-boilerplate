b0509424076c8903f91705fddd3d65badd2be4a2
key kim <keyhyuk.kim@gmail.com>
2021-12-02 02:25:09 +0900

    Refactor init.sh

--- mongodb-replicaset/scripts/init.sh
+++ mongodb-replicaset/scripts/init.sh
@@ -2,4 +2,4 @@
 
 echo "initalize mongo database...."
 sleep 3 
-mongo mongodb://root:1q2w3e4r@10.0.0.17:27020/admin < /scripts/init.js
+mongo mongodb://$MONGO_INITDB_ROOT_USERNAME:$MONGO_INITDB_ROOT_PASSWORD@10.0.0.17:27020/admin < /scripts/init.js
